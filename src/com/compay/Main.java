a9d892d9c470f3b8ef0c3084c895651a9d295d3d
giselMontano <giselalesita@gmail.com>
2022-09-02 16:22:55 -0400

    Solar system ready to be deployed

--- src/com/compay/Main.java
+++ src/com/compay/Main.java
@@ -1,5 +1,12 @@
 package com.compay;
 
 public class Main {
+    public static void main(String[] args) {
+        SolarSystem inception =new SolarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+        System.out.println("The basic structure is ready");
 
+    }
 }
